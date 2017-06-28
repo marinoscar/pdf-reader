f414525dba47cf205313a46a10f6a381a973acf0
Oscar Marin <oscar@marin.cr>
2017-06-27 19:54:41 -0600

    initial commit

---
+++ Code/pdf-reader/Parser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdf_reader
+{
+    public class Parser
+    {
+        public FileInfo File { get; private set; }
+
+    }
+}
