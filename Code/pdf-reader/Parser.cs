1ba85d64faf53a5cbcde868beab4032e463222cd
Oscar Marin <oscar@marin.cr>
2017-07-08 23:43:13 -0600

    improvements

--- Code/pdf-reader/Parser.cs
+++ Code/pdf-reader/Parser.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,6 +12,47 @@ namespace pdf_reader
     public class Parser
     {
         public FileInfo File { get; private set; }
+
+        public Parser(string fileName)
+        {
+            File = new FileInfo(fileName);
+            if (string.IsNullOrWhiteSpace(fileName)
+                || !File.Exists
+                || !File.Extension.ToLowerInvariant().Equals(".pdf"))
+                throw new ArgumentException("Invalid File");
+        }
+
+        public string ToText()
+        {
+            var pdfReader = new PdfReader(File.FullName);
+            var sb = new StringBuilder();
+            for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+            {
+                var strategy = new SimpleTextExtractionStrategy();
+                string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, page, strategy);
+
+                currentText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
+                sb.Append(currentText);
+            }
+            pdfReader.Close();
+            return sb.ToString();
+        }
+
+        public string ToLocationText()
+        {
+            var pdfReader = new PdfReader(File.FullName);
+            var sb = new StringBuilder();
+            for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+            {
+                var strategy = new LocationTextExtractionStrategy();
+                string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, page, strategy);
+
+                currentText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
+                sb.Append(currentText);
+            }
+            pdfReader.Close();
+            return sb.ToString();
+        }
 
     }
 }
--- Code/pdf-reader/Program.cs
+++ Code/pdf-reader/Program.cs
@@ -8,8 +8,28 @@ namespace pdf_reader
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static ConsoleSwitches _args;
+        static void Main(string[] a)
         {
+            _args = new ConsoleSwitches(a);
+            if (_args.ContainsSwitch("/toText")) ToText();
+            Console.WriteLine();
+            Console.WriteLine("Press any key");
+            Console.Read();
+        }
+
+        private static void ToText()
+        {
+            var parser = GetParser();
+            var result = parser.ToLocationText();
+            Console.WriteLine(result);
+        }
+
+
+        private static Parser GetParser()
+        {
+            return new Parser(_args["-f"]);
         }
     }
 }
